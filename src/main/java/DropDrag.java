6fe900c4f23645e088a7fb228e015f38db2718b5
Tolga-dev <orhan.konat.12@gmail.com>
2024-10-26 09:55:38 +0300

    Drop rag example

--- src/main/java/DropDrag.java
+++ src/main/java/DropDrag.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+ 
 public class DropDrag {
     
         public void Run(WebDriver driver) throws InterruptedException {
